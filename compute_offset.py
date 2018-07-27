4bfe079136c2c57b881699795a8083f65bf22cc4
gkabasele <Gnicolas.KN@gmail.com>
2018-07-27 16:10:48 +0200

    copy and modifying offset

--- compute_offset.py
+++ compute_offset.py
@@ -32,7 +32,7 @@ def conv_time(date):
 
     return dateutil.parser.parse(date)
 
-def parse_line(line, state):
+def parse_line(line, state, f):
 
     (number, phys_time, ident, seq, type, snd, rcv) = line.split("|")
     if type == "13":
@@ -46,7 +46,7 @@ def parse_line(line, state):
             state.history[ident][seq]["rcv_remote"] = int(rcv)
             state.offset = compute_offset(state.history[ident][seq])
             state.begin = number
-            start_shifting(state)
+            f.write("{},{},{}\n".format(state.end, state.begin, state.offset))
             state.end = state.begin
         else:
             print "Error, unknown identifier"
@@ -92,7 +92,7 @@ def remove_directory(dirname):
     cmd = ["rm"] + dirname
     subprocess.check_output(cmd)
 
-def worker(fname, result):
+def worker(fname):
 
     # Keep information about time event of the icmp timestamps requests
     history = {}
@@ -107,74 +107,35 @@ def worker(fname, result):
     # A
     dirname = []
 
+    pktNbr = 0
+
     state = State(history, begin, end, offset, dirname, fname)
+
+    f = open("{}_offset.txt".format(fname[:-5]), "w")
     
     cmd = ["tshark", "-r", fname, "-Y", "icmp", "-u","s", "-T", "fields", "-E", "separator=|", "-e", "frame.number" ,"-e", "frame.time", "-e", "icmp.ident", "-e", "icmp.seq", "-e", "icmp.type" ,"-e", "icmp.originate_timestamp", "-e", "icmp.receive_timestamp"]
     tshark = subprocess.check_output(cmd)
     
     line = tshark.split('\n')
+    
+    cmd = ["capinfos", fname]
+    capinfos = subprocess.check_output(cmd)
+    infos = capinfos.split('\n')
 
-
-
-
-
-
+    for info in infos:
+        if "Number of packets = " in info:
+            print info
+            pktNbr = info.split('=')[1] 
+            print pktNbr
 
     
     for info in line[:-1]:
-        parse_line(info, state)
-        if len(state.dirname) > 1:
-            res = merge_trace(state)
-            state.dirname = [res]
+        parse_line(info, state, f)
+
+
+    f.write("{},{},{}\n".format(state.begin, pktNbr, state.offset))
+
+    f.close()
     
-    filename = "{}_{}-last.pcap".format(fname[:-5], state.end)
-    cmd = ["editcap", fname, filename, "0-{}".format(state.end)]
-    res = subprocess.check_output(cmd)
-    
-    new_filename = filename + "_adjusted.pcap"
-    cmd = ["editcap", "-t", str(offset/1000), filename, new_filename]
-    res = subprocess.check_output(cmd)
-    state.dirname.append(new_filename)
-    
-    cmd = ["mergecap", "-w", "{}_adjusted.pcap".format(fname[:-5])] + state.dirname
-    subprocess.check_output(cmd)
 
-    result.append("{}_adjusted.pcap".format(fname[:-5]))
-    
-    remove_file(filename)
-    remove_directory(state.dirname)
-
-# Split the file for each thread
-split_name = "splitted.pcap"
-
-cmd = ["editcap", "-c", args.count, args.file1, split_name]
-subprocess.check_output(cmd)
-
-threads = []
-
-files = []
-
-resFiles = []
-
-for fname in os.listdir(os.getcwd()): 
-    if fname.startswith("splitted"):
-        #start worker
-        files.append(fname)
-        t = threading.Thread(target=worker, args=(fname, resFiles))
-        threads.append(t)
-        t.start()
-
-for t in threads:
-    t.join()
-
-cmd = ["rm"] + files
-subprocess.check_output(cmd)
-# Merge all the resulting file
-
-if len(resFiles) > 1:
-
-    cmd = ["mergecap", "-w", "{}_adjusted.pcap".format(args.file1[:-5])] + resFiles
-    subprocess.check_output(cmd)
-
-if len(resFiles) >= 1:
-    remove_directory(resFiles)
+worker(args.file1)
--- shift_time.c
+++ shift_time.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <pcap.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <string.h>
 
 void parse_line(char *line, ssize_t len ,int *begin, int *end, float *offset) {
 
@@ -46,6 +48,19 @@ void parse_line(char *line, ssize_t len ,int *begin, int *end, float *offset) {
 	}
 }
 
+void change_pkt_offset(struct pcap_pkthdr *header, const u_char* packet, float offset, pcap_dumper_t *pdumper) {
+
+	struct pcap_pkthdr new_header;
+
+	struct pcap_pkthdr *ptr = &new_header;
+
+	memcpy(ptr, header, sizeof(struct pcap_pkthdr));
+
+	(&(ptr->ts))->tv_usec += offset*1000; //millisecond to microsecond
+	pcap_dump((u_char*) pdumper, &new_header, packet);
+
+}
+
 int main(int argc, char **argv) {
 
 	unsigned int packet_counter=0;
@@ -61,9 +76,9 @@ int main(int argc, char **argv) {
 	int end = 0;
 	float offset = 0;
 	
-	if (argc < 2) { 
-			fprintf(stderr, "Usage: %s <pcap>\n", argv[0]); 
-			exit(EXIT_FAILURE); 
+	if (argc < 4) { 
+		fprintf(stderr, "Usage: %s <pcap>\n", argv[0]); 
+		exit(EXIT_FAILURE); 
 	} 
 	
 	pcap_t *handle; 
@@ -71,27 +86,55 @@ int main(int argc, char **argv) {
 	handle = pcap_open_offline(argv[1], errbuf); 
 	
 	if (handle == NULL) { 
-			fprintf(stderr,"Couldn't open pcap file %s: %s\n", argv[1], errbuf); 
-			exit(EXIT_FAILURE); 
-	} 
+		fprintf(stderr,"Couldn't open pcap file %s: %s\n", argv[1], errbuf); 
+		exit(EXIT_FAILURE); 
+	}
+
+	pcap_t *pd;
+	pcap_dumper_t *pdumper;
+	pd = pcap_open_dead(DLT_EN10MB, 65535);	
+
+	if (pd == NULL) {
+		fprintf(stderr, "Couldn't open pcap file %s: %s\n", argv[3], errbuf);	
+		exit(EXIT_FAILURE);
+	}
+
+	pdumper = pcap_dump_open(pd, argv[3]);
+
+	if (pdumper == NULL) {
+		fprintf(stderr, "Couldn't open pcap file %s: %s\n", argv[3], errbuf);	
+		exit(EXIT_FAILURE);
+	}
+	
 
 	fptr = fopen(argv[2], "r");
 	if (fptr == NULL) {
-			fprintf(stderr, "Couldn't open file %s: %s\n", argv[2], errbuf);	
-			exit(EXIT_FAILURE); 
+		fprintf(stderr, "Couldn't open file %s: %s\n", argv[2], errbuf);	
+		exit(EXIT_FAILURE); 
 	}
 
-	while ((read = getline(&line, &len, fptr)) != -1) {
-		parse_line(line, read, &begin, &end, &offset);
-		printf("Begin: %d, End: %d, Offset: %f\n", begin, end, offset);
-	}
+	//while ((read = getline(&line, &len, fptr)) != -1) {
+	//	parse_line(line, read, &begin, &end, &offset);
+	//	printf("Begin: %d, End: %d, Offset: %f\n", begin, end, offset);
+	//}
+
+	read = getline(&line, &len, fptr);			
+	parse_line(line, read, &begin, &end, &offset);
+	
 	
 	while ((packet = pcap_next(handle,&header))) { 
+		change_pkt_offset(&header, packet, offset, pdumper);
+		if (packet_counter == end) {
+			read = getline(&line, &len, fptr);
+			parse_line(line, read, &begin, &end, &offset);
+		}	
 	
-			packet_counter++;
+		packet_counter++;
 	
 	} 
 	pcap_close(handle);
+	pcap_close(pd);
+	pcap_dump_close(pdumper);
 	fclose(fptr);	
 	
 	printf("%d\n", packet_counter);
