39f5985ccd0ca3d43e90a8a549273c7ec2b06f85
gkabasele <Gnicolas.KN@gmail.com>
2018-08-13 14:51:55 +0200

    script to merge the file

--- merge.sh
+++ merge.sh
@@ -1,24 +1,25 @@
-#!/bin/sh
+#!/bin/zsh
 
 
 FILE1=$1
 FILE2=$2
 
-day="${FILE1:0:16}"
+day="${FILE1:0:19}"
+ext="_merge.pcap"
 
 mergecap -w "$day.pcap" $FILE1 $FILE2
 
-offset="merge_$day.txt"
-adjusted="${FILE1:0:16}_adjusted.pcap"
+offset="$day""_merge.txt"
+adjusted="${FILE1:0:25}_adjusted.pcap"
 
 $(python compute_offset.py -f1 "$day.pcap" -f2 $FILE1 -o "$offset")
 
 $(./bin/shift_time -i "$FILE1" -f "$offset" -o "$adjusted")
 
-mergecap -w "$day""_merge.pcap" $adjusted $FILE2
+mergecap -w "$day$ext" $adjusted $FILE2
 
-reordercap -n "$day""_merge.pcap" "$day""_merge_re.pcap"
+reordercap -n "$day$ext" "$day""_merge_re.pcap"
 
+rm "$day$ext"
 
-
-
+mv "$day""_merge_re.pcap" "$day$ext"
