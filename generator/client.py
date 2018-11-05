640e3ba5a521450614528a739558cc74df540a38
gkabasele <gorby.kabasele@uclouvain.be>
2018-11-05 14:01:30 +0000

    starting client/server

--- generator/client.py
+++ generator/client.py
@@ -3,35 +3,71 @@
 import socket
 import sys
 import argparse
+import time
+import cPickle as pickle
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-a", type=str, dest="ip", action="store", help="ip address of the host")
-parser.add_argument("-p", type=int, dest="port", action="store", help="port of the service")
+parser.add_argument("--addr", type=str, dest="ip", action="store", help="ip address of the host")
+parser.add_argument("--port", type=int, dest="port", action="store", help="port of the service")
+parser.add_argument("--dur", type=int, dest="duration", action="store", help="duration of the flow")
+parser.add_argument("--size", type=int, dest="size", action="store", help="size of the flow")
+parser.add_argument("--nbr", type=int, dest="nb_pkt", action="store", help="number_packet send in this flow")
+parser.add_argument("--proto", type=str, dest="tcp", action="store", help="protocol used for the flow")
 args = parser.parse_args()
 
 
 port = args.port
 ip = args.ip
+duration = args.duration
+size = args.size
+tcp = args.tcp
 
-data = " ".join(sys.argv[1:])
-print 'data = %s' %data
 
-# create a TCP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class FlowClient(object):
 
-try:
-        # connect to server 
-        sock.connect((ip, port))
+    """
+        This class represent a client sending request to the server to get request
+        - duration in second
+        - size in byte
+    """
 
-        # send data
-        sock.sendall(bytes(data + "\n"))
+    def __init__(self, server_ip, server_port, duration, size, nb_pkt, TCP=True):
 
-        # receive data back from the server
-        received = str(sock.recv(1024))
-finally:
-        # shut down
-        sock.close()
+        if TCP :
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        print("Sent:     {}".format(data))
-        print("Received: {}".format(received))
+        self.ip = server_ip
+        self.port = server_port
+        self.duration = duration
+        self.size = size
+        self.nb_pkt = nb_pkt
+
+    def run(self):
+        start = time.time()
+        elasped_time = 0
+        nbr_packet = 0
+        recv_size = 0
+        chunk_size = self.size/self.nb_pkt
+        try:
+            # connect to server 
+            sock.connect((ip, port))
+            data = pickle.dumps((self.duration, self.size, self.nb_pkt))
+            sock.sendall(data)
+
+            while recv_size < self.size:
+                # receive data back from the server
+                received = str(sock.recv(chunk_size))
+                recv_size += self.size
+        finally:
+            # shut down
+            sock.close()
+
+
+if __name__ == "__main__":
+
+    client = FlowClient(ip, port, duration, size, tcp == "tcp")) 
+    client.run()
+
--- generator/server.py
+++ generator/server.py
@@ -1,13 +1,27 @@
-import SocketServer
+import logging
+import sys
+import socketserver
 import argparse
+import cPickleas pickle
+import random
+import string
 
+logging.basicConfig(level=logging.DEBUG,
+        format='%(nmae)s:%(message)s',)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-a", type=str, dest="ip", action="store", help="ip address of the host")
-parser.add_argument("-p", type=int, dest="port", action="store", help="port of the service")
+parser.add_argument("--addr", type=str, dest="ip", action="store", help="ip address of the host")
+parser.add_argument("--port", type=int, dest="port", action="store", help="port of the service")
+parser.add_argument("--proto", type=str, dest="tcp", action="store", help="protocol used for the flow")
 args = parser.parse_args()
 
-class MyTCPSocketHandler(SocketServer.BaseRequestHandler):
+port = args.port
+ip = args.ip
+tcp = args.tcp
+
+
+
+class FlowRequestHandler(socketserver.BaseRequestHandler):
     """
     The RequestHandler class for our server.
 
@@ -15,21 +29,69 @@ class MyTCPSocketHandler(SocketServer.BaseRequestHandler):
     override the handle() method to implement communication to the
     client.
     """
+    ALPHA = list(string.printable)
+
+    def __init__(self, request, client_address, server, TCP=True ):
+        self.logger = logging.getLogger('FlowRequestHandler')
+        self.logger.debug('__init__')
+        if TCP:
+            socketserver.TCPRequestHandler.__init__(self, request, client_address,
+                                                    server) 
+        else:
+            socketserver.UDPRequestHandler.__init__(self, request, client_address,
+                                                    server)
+        self.size = None
+        self.duration = None
+        self.nb_pkt = None
+
+
+    @classmethod
+    def create_chunk(cls, size):
+        s = ""
+        for x in range(size):
+            s += cls.ALPHA[random.randint(0, len(cls.ALPHA)-1)]  
+
+        return bytes(s) 
+
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        #If initial request compute retrieve chunk size
+         
+        data = pickle.load(self.request.recv())
+        self.duration, self.size, self.nb_pkt = data
+       
+        chunk_size = self.size/self.nb_pkt 
+        sended_size = 0
+
+        while sended_size < size:
+            self.request.sendall(FlowRequestHandler.create_chunk(chunk_size))
+            # wait based on duration
+            sended_size += chunk_size
+        
+class FlowServer(socketsever.BaseServer):
+
+    def __init__(self, server_address,
+                 TCP=True,
+                 handler_class=FlowRequestHandler,):
+        self.logger = logging.getLogger('FlowServer')
+        self.logger.debug('__init__')
+        self.is_tcp = TCP
+        if TCP:
+            socketserver.TCPServer.__init__(self, server_address, 
+                                            handler_class)
+        else:
+            socketserver.UDPServer.__init__(self, server_address,
+                                            handler_class)
+
+    def finish_request(self, request, client_address):
+        self.RequestHandlerClass(request, client_address, self, is_tcp)
+        
 
 if __name__ == "__main__":
                  
-    HOST, PORT = args.ip, args.port
     
     # instantiate the server, and bind to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPSocketHandler)
+    server = FlowServer((ip, port),tcp == "tcp" ,FlowRequestHandler)
     
     # activate the server
     # this will keep running until Ctrl-C
