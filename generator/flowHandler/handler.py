fe6d8d690b3f805c0fd91c9b60a3e63fa4ae3fd2
gkabasele <Nicolas.houtain@actito.com>
2018-11-20 16:56:36 +0000

    read all file

--- generator/flowHandler/handler.py
+++ generator/flowHandler/handler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import struct
+import os
 import argparse
 import socket
 from ctypes import sizeof
@@ -94,55 +95,60 @@ class FlowHandler(object):
 
     def __init__(self, filename):
 
+        self.index = 0
         self.flows = self.retrieve_flows(filename)
+
+    def read(self, _type, readsize, f):
+        self.index += readsize
+        return struct.unpack(_type, f.read(readsize))[0] 
 
     def retrieve_flows(self, filename):
         flows = []
         with open(filename, "rb") as f:
-            #srcip = struct.unpack('I', f.read(4))[0]
-            srcip = f.read(4).encode('hex')
-            #dstip = struct.unpack('I', f.read(4))[0]
-            dstip = f.read(4).encode('hex') 
-            sport = struct.unpack('H', f.read(2))[0]
-            dport = struct.unpack('H', f.read(2))[0]
-            proto = struct.unpack('B', f.read(1))[0]
-            padding = struct.unpack('BBB', f.read(3))[0]
+            filesize = os.path.getsize(filename)
+            while self.index < filesize:
+                srcip = self.read('I', 4, f)
+                dstip = self.read('I', 4, f)
+                sport = self.read('H', 2, f)
+                dport = self.read('H', 2, f)
+                proto = self.read('B', 1, f)
+                self.read('BBB', 3, f)
 
-            size = struct.unpack('Q', f.read(8))[0]
-            print size
+                size = self.read('Q', 8, f)
+                print size
 
-            nb_pkt = struct.unpack('Q', f.read(8))[0]
-            print nb_pkt
+                nb_pkt = self.read('Q', 8, f)
+                print nb_pkt
 
-            first_sec = struct.unpack('Q', f.read(8))[0]
-            first_micro = struct.unpack('Q', f.read(8))[0]
-            first = (first_sec, first_micro)
+                first_sec = self.read('Q', 8, f)
+                first_micro = self.read('Q', 8, f)
+                first = (first_sec, first_micro)
 
-            duration = struct.unpack('f', f.read(4))[0]
-            print duration
+                duration = self.read('f', 4, f)
+                print duration
 
-            size_list = struct.unpack('Q', f.read(8))[0]
-            print size_list
+                size_list = self.read('Q', 8, f)
+                print size_list
 
-            pkt_dist = []
-            while size_list > 0:
-                val = struct.unpack('H', f.read(2))[0]
-                pkt_dist.append(val)
-                size_list -= 1
-            print pkt_dist
+                pkt_dist = []
+                while size_list > 0:
+                    val = self.read('H', 2, f)
+                    pkt_dist.append(val)
+                    size_list -= 1
+                print pkt_dist
 
-            size_list = struct.unpack('Q', f.read(8))[0]
-            print size_list
-            arr_dist = []
-            while size_list > 0:
-                val = struct.unpack('f', f.read(4))[0]
-                arr_dist.append(val)
-                size_list -= 1
-            print arr_dist
+                size_list = self.read('Q', 8, f)
+                print size_list
+                arr_dist = []
+                while size_list > 0:
+                    val = self.read('f', 4, f)
+                    arr_dist.append(val)
+                    size_list -= 1
+                print arr_dist
 
-            flow = Flow(srcip, dstip, sport, dport, proto, first, duration,
+                flow = Flow(srcip, dstip, sport, dport, proto, first, duration,
                         size, nb_pkt, pkt_dist, arr_dist)
-            flows.append(flow)
+                flows.append(flow)
         return flows
 
     def connect_to_network(self, ip, port):
