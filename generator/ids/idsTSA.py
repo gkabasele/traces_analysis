38f0d3b73bcfddd9f46e7a62ba0590dcc6667d37
gkabasele <Nicolas.houtain@actito.com>
2019-08-22 14:31:00 +0000

    testing EWMA

--- generator/ids/idsTSA.py
+++ generator/ids/idsTSA.py
@@ -3,13 +3,16 @@ import sys
 import os
 import argparse
 import logging
+import pdb
+import math
 from datetime import datetime
 from datetime import timedelta
 from collections import OrderedDict
 import numpy as np
+import matplotlib.pyplot as plt 
+from decimal import *
 
 REG =r"(?P<ts>(\d+\.\d+)) IP (?P<src>(?:\d{1,3}\.){3}\d{1,3})(\.(?P<sport>\d+)){0,1} > (?P<dst>(?:\d{1,3}\.){3}\d{1,3})(\.(?P<dport>\d+)){0,1}: (?P<proto>(tcp|TCP|udp|UDP|icmp|ICMP))( |, length )(?P<size>\d+){0,1}"
- 
 
 TS = "ts"
 SRC = "src"
@@ -56,12 +59,17 @@ class FlowRecord(object):
         self.ts = ts
 
     def __hash__(self):
-        return hash((self.src, self.dst, self.dport))
+        return hash((self.src, self.sport, self.dst, self.dport, self.proto))
+
+    def __eq__(self, other):
+        return hash(other) == self.__hash__()
 
     def __str__(self):
         return "{}:{}->{}:{} ({})".format(self.src, self.sport,
                                           self.dst, self.dport,
                                           self.proto)
+    def __repr__(self):
+        return self.__str__()
 
 class FlowCreationCounter(object):
 
@@ -100,7 +108,10 @@ class FlowCreationCounter(object):
         new_flow = []
         counter = 0
         index_period = 0
-        flow_array = {}
+        flow_array = OrderedDict()
+        # if there is a gap larger than a period between two consecutive seen flows 
+        # (Does not occur generally as it means no packet for 1 periods)
+        tmp_flow = None
         for trace in listdir:
             filename = os.path.join(self.dirname, trace)
             with open(filename, "r") as f:
@@ -109,22 +120,130 @@ class FlowCreationCounter(object):
                     if flow is None:
                         continue
                     if start_time is None and end_time is None:
-                        start_time = f.ts
-                        end_time = f.ts + self.period_size
+                        start_time = flow.ts
+                        end_time = flow.ts + self.period_size
 
-                    if f.ts < end_time:
-                        if f not in flow_array:
-                            flow_array[f] = index_period
-                            counter += 1
-                    else:
+                    if flow.ts > end_time:
                         new_flow.append(counter)
                         self.clear_array(flow_array, index_period)
                         start_time = end_time
                         end_time = start_time + self.period_size
                         counter = 0
                         index_period += 1
-                        if f.ts < end_time:
-                            if f not in flow_array:
-                                flow_array[f] = index_period
-                                counter += 1
+
+                    if flow.ts < end_time:
+                        if flow not in flow_array:
+                            flow_array[flow] = index_period
+                            counter += 1
+                        else:
+                            flow_array[flow] = index_period
+
         return np.array(new_flow)
+
+class TSAnalyzer(object):
+    
+    # Implementation of paper: Towards Real-Time Intrusion Detection for Netflow
+    # and IPFIX
+
+    def __init__(self, alpha, cthresh, big_M, csum, nbr_interval):
+        self.alpha = alpha
+        self.cthresh = cthresh
+        self.csum = csum
+        self.big_M = big_M
+        self.last_val = None
+        self.observed_val = None # observed in t
+        self.weighted_val = None # weighted for t
+        self.forecasted = None   # forecasted for t+1
+        self.upper = None
+        self.thresh_sum = 0
+        self.uppersum = None
+        self.errors = []
+        self.errors_std = None
+        self.nbr_interval = nbr_interval
+
+    def estimate_next(self, obs):
+        self.estimated_val = self.alpha * obs + (1-self.alpha)*self.last_val
+        self.forecasted = self.estimated_val
+        self.last_val = self.estimated_val
+
+    def compute_error(self, obs):
+        if len(self.errors) >= self.nbr_interval:
+            self.errors.pop(0)
+
+        self.errors.append(obs - self.forecasted)
+        self.errors_std = np.std(self.errors)
+        self.upper = self.forecasted + max(self.cthresh*self.errors_std,
+                                              self.big_M)
+
+    def compute_cumsum(self, obs):
+        self.thresh_sum = max(self.thresh_sum + (obs - self.upper), 0)
+        self.uppersum = self.csum * self.errors_std
+
+def main(dirname):
+    interval_size = 5
+    exporter = FlowCreationCounter(interval_size, re.compile(REG), dirname)
+    ts_creation_flow = exporter.new_flow_ts()
+    span = 15
+    N = math.ceil(span/interval_size) 
+    alpha = float(2/(N+1))
+    cthresh = 1
+    csum = 7
+    big_M = 0
+    analyzer = TSAnalyzer(alpha, cthresh, big_M, csum, N)
+    analyzer.last_val = ts_creation_flow[0]
+    forecasted_values = [analyzer.last_val]
+    thresh_sum = []
+    uppersum = []
+    for i in range(len(ts_creation_flow)-1):
+        x_t = ts_creation_flow[i]
+        x_next = ts_creation_flow[i+1]
+        analyzer.estimate_next(x_t)
+        forecasted_values.append(analyzer.forecasted)
+        analyzer.compute_error(x_next)
+        analyzer.compute_cumsum(x_next)
+        thresh_sum.append(analyzer.thresh_sum)
+        uppersum.append(analyzer.uppersum)
+        if analyzer.thresh_sum > analyzer.uppersum:
+            print("Warn:forecasted:{},observed:{}".format(forecasted_values[i+1],
+                                                          x_next))
+    print("LR: {}, LF: {}".format(len(ts_creation_flow),
+                                  len(forecasted_values)))
+    plot(ts_creation_flow, forecasted_values)
+
+    plot(thresh_sum, uppersum)
+def test_ewma():
+
+    ts = [1, -0.5, 0.0, -0.8, -0.8, -1.2, 1.5, -0.6, 1, -0.9, 1.2, 0.5, 2.6,
+          0.7, 1.1, 2.0]
+
+    estimated = [0.250, 0.063, 0.047, -0.165, -0.324, -0.543, -0.033, -0.175,
+                 0.120, -0.136, 0.199, 0.274, 0.856, 0.817, 0.888, 1.166]
+    alpha = 0.25
+    cthresh = 1.5
+    big_M = 5000
+    csum = 5
+    nbr_interval = 5
+
+    analyzer = TSAnalyzer(alpha, cthresh, big_M, csum, nbr_interval)
+    analyzer.last_val = 0
+    computed = []
+    for i, x in enumerate(ts):
+        try:
+            analyzer.estimate_next(x)
+            res = Decimal(analyzer.estimated_val).quantize(Decimal('.001'),
+                                                           rounding=ROUND_UP)
+            expect = Decimal(str(estimated[i]))
+            assert res == expect
+        except AssertionError:
+            print("AssertionError: expected: {}, got: {}".format(expect, res))
+
+def plot(data, forecasted_data=None):
+    xs = np.arange(len(data))
+    plt.plot(xs, data)
+    if forecasted_data is not None:
+        plt.plot(xs, forecasted_data)
+    plt.show()
+
+if __name__== "__main__":
+    #test_ewma()
+    main(indir)
