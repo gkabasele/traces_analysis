5018e2d2ecd934526ae562bb8e7792933421ccb1
gkabasele <Nicolas.houtain@actito.com>
2019-08-23 11:53:23 +0000

    uncommitted comit new test for tsa

--- generator/ids/idsTSA.py
+++ generator/ids/idsTSA.py
@@ -237,6 +237,24 @@ def test_ewma():
         except AssertionError:
             print("AssertionError: expected: {}, got: {}".format(expect, res))
 
+    # from https://school.stockcharts.com/doku.php?id=technical_indicators:moving_averages
+
+    ts = [22.27, 22.19, 22.08, 22.17, 22.18, 22.13, 22.23, 22.43, 22.24, 22.29,
+          22.15, 22.39, 22.38, 22.61, 23.36, 24.05, 23.75, 23.83, 23.95, 23.63,
+          23.82, 23.87, 23.65, 23.19, 23.10, 23.33, 22.68, 23.10, 22.50, 22.17]
+
+    estimated = [22.22, 22.21, 22.24, 22.27, 22.33, 22.52, 22.80, 22.97, 23.13,
+                 23.28, 23.34, 23.43, 23.51, 23.54, 23.47, 23.40, 23.39, 23.26,
+                 23.23, 23.08, 22.92]
+
+    alpha = 0.1818
+    analyzer = TSAnalyzer(alpha, cthresh, big_M, csum, nbr_interval)
+    analyzer.last_val = np.mean(ts[0:10])
+    forecasted = [analyzer.last_val]
+    for i in range(10, len(ts)-1):
+        analyzer.estimate_next(ts[i])
+        forecasted.append(analyzer.forecasted)
+    plot(ts[10:], forecasted)
 def plot(data, forecasted_data=None):
     xs = np.arange(len(data))
     plt.plot(xs, data)
@@ -245,5 +263,5 @@ def plot(data, forecasted_data=None):
     plt.show()
 
 if __name__== "__main__":
-    #test_ewma()
-    main(indir)
+    test_ewma()
+    #main(indir)
