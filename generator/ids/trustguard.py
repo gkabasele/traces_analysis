8c095e1c94c0844e79387ed6dd8ce5e02c5e16a6
gkabasele <Nicolas.houtain@actito.com>
2019-09-09 15:59:16 +0000

    testing trustguard

--- generator/ids/trustguard.py
+++ generator/ids/trustguard.py
@@ -1,6 +1,7 @@
 import re
 import os
 import math
+import pdb
 import argparse
 from collections import OrderedDict
 from datetime import datetime, timedelta
@@ -77,7 +78,7 @@ class Tprofile(object):
     def compute_entropy(self):
         sigma = entropy(self.probs, base=2)
         # max level + 1 because it starts at 1
-        imax = - math.sqrt(self.get_max_pkt_size_level()+1)
+        imax = math.sqrt(self.get_max_pkt_size_level()+1)
         return imax * sigma
 
     def __str__(self):
@@ -88,7 +89,7 @@ class Tprofile(object):
 
 class TrustGuard(object):
 
-    def __init__(self, reg, period_size, pkt_bins, quiet=True, thresh=0.2):
+    def __init__(self, reg, period_size, pkt_bins, quiet=True, thresh=0.5):
 
         self.reg = reg
         self.quiet = quiet
@@ -99,6 +100,7 @@ class TrustGuard(object):
         self.end = None
 
         self.thresh = thresh
+        self.min_threshold = None
 
         self.period = timedelta(seconds=period_size)
         self.current_interval = 0
@@ -106,11 +108,14 @@ class TrustGuard(object):
         self.mal_interval = []
 
     def add_pkt_size(self, dip, pkt_size):
-
         if dip not in self.profiles:
             self.profiles[dip] = Tprofile(self.pkt_bins)
 
         self.profiles[dip].add(pkt_size)
+    
+    def clear_profile(self):
+        for v in self.profiles.values():
+            v.clear_counter()
 
     def _getdata(self, line):
         try:
@@ -128,15 +133,23 @@ class TrustGuard(object):
         except AttributeError:
             raise ValueError("Could parse line: {}".format(line))
 
-    def run_detection(self):
+    def run_detection(self, debug=False):
+        if debug:
+            pdb.set_trace()
         under_attack = False
-        for k, v in self.profiles:
+        for k, v in self.profiles.items():
+            v.compute_dist()
             entropy = v.compute_entropy()
             if entropy < self.thresh:
                 under_attack = True
                 if not self.quiet:
                     print("Address IP {} under attack".format(k))
-            v.clear()
+                if under_attack:
+                    self.mal_interval.append(self.current_interval)
+            if self.min_threshold is None or entropy < self.min_threshold:
+                self.min_threshold = entropy
+
+            v.clear_counter()
 
     def run(self, dirname): 
         listdir = sorted(os.listdir(dirname))
@@ -166,7 +179,52 @@ class TrustGuard(object):
                     self.add_pkt_size(dst, size)
 
 def test_trustguard():
-    pass
+    ipa = "10.0.0.1"
+    ipb = "10.0.0.2"
+
+    ids = TrustGuard(reg=None, period_size=5, pkt_bins=PKT_SIZE_LVL,
+                     quiet=False)
+    
+    pkt_size_a = [300, 694, 59, 1200]
+    proba = [0.2, 0.4, 0.3, 0.1]
+
+    assert len(pkt_size_a) == len(proba)
+
+    pkt_size_b = [193, 1300, 653, 1145, 439, 893]
+    probb = [0.12, 0.08, 0.27, 0.08, 0.2, 0.25] 
+
+    assert len(pkt_size_b) == len(probb)
+
+    print("Normal behavior")
+    for i in range(1, 1000):
+        if i % 50 == 0:
+            ids.run_detection()
+            ids.current_interval += 1
+
+        size_a = np.random.choice(pkt_size_a, p=proba) 
+        size_b = np.random.choice(pkt_size_b, p=probb)
+        ids.add_pkt_size(ipa, size_a)
+        ids.add_pkt_size(ipb, size_b)
+
+    ids.clear_profile()
+
+    ids.thresh = ids.min_threshold
+
+    print("Min Thresh:{}".format(ids.min_threshold))
+
+    pkt_size_a = [20, 300, 694, 59, 1200]
+    proba = [0.90, 0.02, 0.04, 0.03, 0.01]
+    assert len(pkt_size_a) == len(proba)
+    print("Attack behavior")
+    for i in range(1, 1000):
+        if i % 50 == 0:
+            ids.run_detection()
+            ids.current_interval += 1
+
+        size_a = np.random.choice(pkt_size_a, p=proba)
+        size_b = np.random.choice(pkt_size_b, p=probb)
+        ids.add_pkt_size(ipa, size_a)
+        ids.add_pkt_size(ipb, size_b)
 
 def main(indir):
 
@@ -179,4 +237,5 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     indir = args.indir
-    main(indir)
+    #main(indir)
+    test_trustguard()
